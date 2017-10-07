71242749f6470e3ca7d66a4c84c1fd67dfcff838
Ivan Veselov <veselov.iva@gmail.com>
2017-10-07 22:11:51 +0300

    Add fields to Token class.

--- src/main/java/ru/spbau/bachelors2015/veselov/llexer/Symbol.java
+++ src/main/java/ru/spbau/bachelors2015/veselov/llexer/Symbol.java
@@ -1,4 +1,7 @@
 package ru.spbau.bachelors2015.veselov.llexer;
 
 public class Symbol extends Token {
+    public Symbol() {
+        super(0, 0, 0);
+    }
 }
--- src/main/java/ru/spbau/bachelors2015/veselov/llexer/Token.java
+++ src/main/java/ru/spbau/bachelors2015/veselov/llexer/Token.java
@@ -1,4 +1,27 @@
 package ru.spbau.bachelors2015.veselov.llexer;
 
 public abstract class Token {
+    private final int line;
+
+    private final int vpos;
+
+    private final int length;
+
+    public Token(final int line, final int vpos, final int length) {
+        this.line = line;
+        this.vpos = vpos;
+        this.length = length;
+    }
+
+    public int line() {
+        return line;
+    }
+
+    public int vpos() {
+        return vpos;
+    }
+
+    public int length() {
+        return length;
+    }
 }
